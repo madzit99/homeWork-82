f709b8cdcadab194a3cdb0007f1110b73b940556
Aliev Madjit <madzitaliev51@gmail.com>
2024-09-04 17:54:42 +0600

    HW 82: Сделал модель User и Trackhistory

---
+++ api/models/TrackHistory.ts
@@ -0,0 +1,22 @@
+import mongoose from "mongoose";
+
+const Schema = mongoose.Schema;
+
+const TrackHistoryShema = new mongoose.Schema({
+  user: {
+    type: String,
+    required: true,
+  },
+  trackId: {
+    type: String,
+    required: true,
+  },
+  datetime: {
+    type: Date,
+    required: true,
+  },
+});
+
+const TrackHistory = mongoose.model("TrackHistory", TrackHistoryShema);
+
+export default TrackHistory;

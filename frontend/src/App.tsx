50378226e6eb571ceb54faba8c59c116a8626305
Aliev Madjit <madzitaliev51@gmail.com>
2024-09-12 17:28:28 +0600

    Создал нужные файлы constants axiosApi AppToolbar store hooks

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,15 @@
+import AppToolbar from "./UI/AppToolbar/AppToolbar";
 
 const App = () => {
 
   return (
     <>
+      <header>
+        <AppToolbar />
+      </header>
+      
     </>
-  )
+  );
 }
 
 export default App
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,18 @@
-import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+import { Provider } from 'react-redux';
+import { BrowserRouter } from 'react-router-dom';
+import { store } from './app/store.ts';
+import theme from './theme.ts';
+import { ThemeProvider } from '@mui/material';
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+createRoot(document.getElementById("root")!).render(
+    <Provider store={store}>
+      <BrowserRouter>
+        <ThemeProvider theme={theme}>
+          <App />
+        </ThemeProvider>
+      </BrowserRouter>
+    </Provider>,
+);
